d61d6b8e26057d32403dfee122b33b8ab4ac355b
Jacinta Calixte <jacinta246@yahoo.com>
2020-03-11 03:59:30 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz (number)
- if number % 3==0
+ if number % 3 and 5==0
+   return "FizzBuzz"
+ elsif number % 3==0
    return "Fizz"
 elsif number % 5==0
    return "Buzz"
- elsif number % 3 and 5==0
-   return "FizzBuzz"
  end
 end
